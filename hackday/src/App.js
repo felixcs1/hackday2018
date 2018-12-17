1f9649373dd4029cfcfec0cb9e481e339194f4a1
Matt Greenham <matt.greenham@bbc.co.uk>
2018-12-17 10:33:45 +0000

    Add routes

--- hackday/src/App.js
+++ hackday/src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import { Button } from 'reactstrap';
-import logo from './logo.svg';
 import './App.css';
-import QuestionContainer from './components/question-container';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Staff from './components/staff/Staff'
+import User from './components/user/User'
+
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          HELLO - app 
-        </header>
-        <QuestionContainer></QuestionContainer>
-      </div>
+        <Router>
+          <div className="App">
+            <header className="App-header">
+              HELLO - app
+            </header>
+              <Route exact path="/user" component={User} />
+              <Route exact path="/staff" component={Staff} />
+          </div>
+        </Router>
     );
   }
 }
